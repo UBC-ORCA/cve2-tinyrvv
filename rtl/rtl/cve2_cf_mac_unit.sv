778b8399fe8dccc10457f49665baa5b5efce14a6
Steven Chin <skingtw@gmail.com>
2026-07-12 17:37:33 +0800

    scaling test

--- rtl/rtl/cve2_cf_mac_unit.sv
+++ rtl/rtl/cve2_cf_mac_unit.sv
@@ -276,7 +276,7 @@ assign data_wdata_o = mem_wdata;
 .scale0_o(scale0),
  .scale1_o(scale1),
  .scale2_o(scale2),
- //.scale3_o(scale3),
+ .scale3_o(scale3),
  .scale_col_i(scale_col),
     .scale_row_sel_i(scale_row_sel)
 //SCALE_end
@@ -322,7 +322,7 @@ mac_scale_accum u_scale_accum3 (
 //to_RM
 assign scale_col = 0;
 assign scale_row_sel = 0;
-assign scale3 = 127;
+//assign scale3 = 127;
  
 //------------------------------------------------------------
 // Temporary tie-offs
@@ -330,19 +330,19 @@ assign scale3 = 127;
 
 always_comb begin
     scaleA[0] = 8'h7F;
-    scaleA[1] = 8'h7F;
-    scaleA[2] = 8'h7F;
-    scaleA[3] = 8'h7F;
+    scaleA[1] = 8'h80;
+    scaleA[2] = 8'h7E;
+    scaleA[3] = 8'h80;
 
     scaleB[0] = 8'h7F;
     scaleB[1] = 8'h7F;
     scaleB[2] = 8'h7F;
-    scaleB[3] = 8'h7F;
+    scaleB[3] = 8'h80;
 
-    scale_accum_in[0] = 16'hC0A0;
+    scale_accum_in[0] = 16'h4302;
     scale_accum_in[1] = 16'h4302;
-    scale_accum_in[2] = 16'h42FE;
-    scale_accum_in[3] = 16'h42FE;
+    scale_accum_in[2] = 16'h4302;
+    scale_accum_in[3] = 16'h4302;
 end
 //to_RM_end
 
